38f919f94c1defc07fcf34649f24b967bd67e787
Mikhail Bulankin <mvl.bulankin@gmail.com>
2024-01-15 21:16:45 +0300

    fix data types

--- main.py
+++ main.py
@@ -5,38 +5,77 @@ import pandas as pd
 
 
 def parse():
-    files = glob.glob("/home/mvl/work/hh_parser/download_pages/*.html")
+    files = glob.glob(r"C:\work\hh_parser\download_pages_fix\*.html")
 
     df = pd.DataFrame(
-        columns=["industry", "region", "city", "vacancy_dynamics", "resume_dynamics", "hh_index", "salary"],
+        columns=[
+            "last_day_mnth",
+            "industry",
+            "region",
+            "city",
+            "vacancy_dynamics",
+            "resume_dynamics",
+            "hh_index",
+            "median_salary"
+        ],
     )
 
     for file in files:
-        with open(f"{file}") as f:
+        with open(f"{file}", encoding="utf-8") as f:
             src = f.read()
 
-            soup = BeautifulSoup(src, "lxml")
+        soup = BeautifulSoup(src, "lxml")
+        last_day_mnth = "2023-03-31"
+        industry = soup.find("div", class_="_comparisonTableTitle_1rq97_121").text.strip()
+        rows = soup.find_all(class_="_comparisonTableRow_1rq97_118 _comparisonTableContent_1rq97_171")
 
-            industry = soup.find("div", class_="_comparisonTableTitle_1rq97_121").text.strip()
+        for row in rows:
+            columns = row.find_all("div")
 
-            rows = soup.find_all(class_="_comparisonTableRow_1rq97_118 _comparisonTableContent_1rq97_171")
+            region = columns[0].text.strip()
+            city = columns[1].text.strip()
 
-            for row in rows:
-                columns = row.find_all("div")
+            vacancy_dynamics = columns[2].text.strip().replace("%", "")
+            if vacancy_dynamics != "-":
+                vacancy_dynamics = int(vacancy_dynamics)
+            else:
+                vacancy_dynamics = None
 
-                new_row = pd.DataFrame({
-                    "industry": industry,
-                    "region": columns[0].text.strip(),
-                    "city": columns[1].text.strip(),
-                    "vacancy_dynamics": columns[2].text.strip(),
-                    "resume_dynamics": columns[3].text.strip(),
-                    "hh_index": columns[4].text.split("/")[0].strip().replace(".", ","),
-                    "salary": columns[5].text.split("/")[0].strip(),
-                }, index=[0])
+            resume_dynamics = columns[3].text.strip().replace("%", "")
+            if resume_dynamics != "-":
+                resume_dynamics = int(resume_dynamics)
+            else:
+                resume_dynamics = None
 
-                df = pd.concat([df, new_row], ignore_index=True)
+            hh_index = columns[4].text.split("/")[0].strip()
+            if hh_index != "-":
+                hh_index = float(hh_index)
+            else:
+                hh_index = None
 
-    df.to_excel("output.xlsx", index=False)
+            median_salary = columns[5].text.split("/")[0].strip().replace(",", ".")
+            if median_salary != "-":
+                median_salary = float(median_salary)
+            else:
+                median_salary = None
+
+            new_row = pd.DataFrame({
+                "last_day_mnth": last_day_mnth,
+                "industry": industry,
+                "region": region,
+                "city": city,
+                "vacancy_dynamics": vacancy_dynamics,
+                "resume_dynamics": resume_dynamics,
+                "hh_index": hh_index,
+                "median_salary": median_salary,
+            }, index=[0])
+
+            df = pd.concat([df, new_row], ignore_index=True)
+
+    df.to_excel("hh_data_2023_03_finance.xlsx", index=False)
+    df.to_csv("hh_data_2023_03_finance.csv", index=False)
+
+    print("All data loaded")
 
 
 if __name__ == "__main__":
